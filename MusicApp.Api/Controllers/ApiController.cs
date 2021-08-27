40f22c0f238f119eb63a951ccf4e30c7d0b8390f
Calvin Dalenta <calvindalentark@gmail.com>
2021-08-27 23:47:00 +0700

    Implemented API for GetAudioImage

--- MusicApp.Api/Controllers/ApiController.cs
+++ MusicApp.Api/Controllers/ApiController.cs
@@ -23,7 +23,7 @@ namespace MusicApp.Api.Controllers
         }
 
         [HttpGet, Route("track/{id}")]
-        public IActionResult Music(string id)
+        public IActionResult GetAudioFile(string id)
         {
             // Check if the id exists
             Track track = _tracks.Find(track => track.Id == id);
@@ -33,8 +33,8 @@ namespace MusicApp.Api.Controllers
             }
 
             // Resolve the file path
-            // Should resolve to {ApplicationPath}/{genre}/{audio.mp3}
-            string relativePath = Path.Combine(track.Genre.ToLower(), track.FileName);
+            // Should resolve to {ApplicationPath}/audio/{genre}/{audio.mp3}
+            string relativePath = Path.Combine("audio", track.Genre.ToLower(), track.FileName);
             string fullPath = PathUtils.GetFilePathFromApplicationPath(relativePath);
 
             // Return the file stream
@@ -42,8 +42,30 @@ namespace MusicApp.Api.Controllers
             return File(stream, "audio/mp3", true);
         }
 
+        [HttpGet, Route("image/{id}")]
+        public IActionResult GetAudioImage(string id)
+        {
+            // Check if the id exists
+            Track track = _tracks.Find(track => track.Id == id);
+            if (track == null)
+            {
+                return NotFound("No track found for the specified id");
+            }
+
+            // Resolve the file path
+            // Should resolve to {ApplicationPath}/images/{genre}/{audio.jpeg}
+            string fileName = Path.GetFileNameWithoutExtension(track.FileName) + ".jpeg";
+            string relativePath = Path.Combine("images", track.Genre.ToLower(), fileName);
+            string fullPath = PathUtils.GetFilePathFromApplicationPath(relativePath);
+
+            // Return the file stream
+            string mimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg;
+            FileStream stream = System.IO.File.OpenRead(fullPath);
+            return File(stream, mimeType, true);
+        }
+
         [HttpGet, Route("tracks")]
-        public IActionResult Tracks()
+        public IActionResult GetAllTracks()
         {
             return Ok(_tracks.ToArray());
         }
